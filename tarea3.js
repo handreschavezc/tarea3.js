1f0427cdd4c7400a5be14e152c0322de7bc783df
handreschavezc <43646713+handreschavezc@users.noreply.github.com>
2021-03-01 03:07:03 -0300

    Update tarea3.js

--- tarea3.js
+++ tarea3.js
@@ -1,1 +1,16 @@
+//Función que me permita encontrar todas las ordenes que contengan de ingredients lechuga y palta
 
+const findOrderOfBreads = () => {
+    const result = filterOrdersByType({ type: 'ingredients', filter: 'lechuga','palta'})
+}
+
+//Función que me permita encontrar una orden a través del id y 
+//que me indique si la orden incluye pepinillos entre sus ingredients, debería devolver un true|false como respuesta.
+const findOrderOfingredients = (id) => {
+    const result = filterOrdersByType({ type: 'ingredients', filter: 'pepinillos'})
+                    .find(sandwichOrder => sandwichOrder.id === id)
+    if(result){
+        return true
+    }
+    return false
+}
